9349150a31110b862acbaf8de655a8d76b2d55db
Divakhar31 <divakhar3108@gmail.com>
2026-05-25 21:48:32 +0800

    db.py

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 import mysql.connector
+import os
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="uthm_lms",
-    port=3307   
-)
-
+# ====================== DB CONNECTION ======================
+def get_db():
+    return mysql.connector.connect(
+        host=os.environ.get('DB_HOST', 'localhost'),
+        user=os.environ.get('DB_USER', 'root'),
+        password=os.environ.get('DB_PASSWORD', ''),
+        database=os.environ.get('DB_NAME', 'uthm_lms'),
+        port=int(os.environ.get('DB_PORT', 3307))
+    )
